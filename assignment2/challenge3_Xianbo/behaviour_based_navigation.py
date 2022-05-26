7fb0cf2e199670cddbb9815805915ff6a4c699ed
Xianbo XU <Silas_X@outlook.com>
2022-05-26 15:22:58 +0200

    Use tanh as the velocity fuction

--- assignment2/challenge3_Xianbo/behaviour_based_navigation.py
+++ assignment2/challenge3_Xianbo/behaviour_based_navigation.py
@@ -23,7 +23,6 @@ def FObstacle(obs_distance, obs_angle):
     else:
         Fobs=0
     return Fobs
-
 def FStochastic():
     """FStochastic adds noise to the turnrate force. This is just to make the simulation more realistic by adding some noie something useful here"""
     Kstoch=0.03
@@ -36,19 +35,24 @@ def FOrienting():
     Forient=0
     return Forient
 
+
+
 def compute_velocity(sonar_distance_left, sonar_distance_right):
     max_velocity = 1.0
     max_distance = 0.5 #m
     min_distance = 0.2 #m
 
-    if sonar_distance_left>max_distance and sonar_distance_right > max_distance:
-        velocity = max_velocity
-    elif sonar_distance_left<min_distance or sonar_distance_right < min_distance:
-        velocity = 0.0
-    elif sonar_distance_left<sonar_distance_right:
-        velocity = max_velocity*sonar_distance_left/max_distance
-    else:
-        velocity = max_velocity*sonar_distance_right/max_distance
+    # if sonar_distance_left>max_distance and sonar_distance_right > max_distance:
+        # velocity = max_velocity
+    # elif sonar_distance_left<min_distance or sonar_distance_right < min_distance:
+        # velocity = 0.0
+    # elif sonar_distance_left<sonar_distance_right:
+        # velocity = max_velocity*sonar_distance_left/max_distance
+    # else:
+        # velocity = max_velocity*sonar_distance_right/max_distance
+    # else:
+        # velocity=max_velocity*(sonar_distance_left+sonar_distance_right)/2
+    velocity=math.tanh(0.03*((sonar_distance_left+sonar_distance_right)/2-(max_distance+min_distance)/2))+1
 
     
     return velocity
@@ -57,7 +61,7 @@ def compute_turnrate(target_dist, target_angle, sonar_distance_left, sonar_dista
     max_turnrate = 0.349 #rad/s # may need adjustment!
     # max_turnrate = 3.145926 /2  #rad/s # may need adjustment!
 
-    delta_t = 1 # may need adjustment!
+    delta_t = 0.05 # may need adjustment!
     sonar_angle_left = 30 * degree
     sonar_angle_right = -30 * degree
     beta_1=20
@@ -67,7 +71,7 @@ def compute_turnrate(target_dist, target_angle, sonar_distance_left, sonar_dista
     print("Fobs_left",Fobs_left)
     print("Fobs_right",Fobs_right)
     FTotal = 0.5*FTarget(target_dist, target_angle) + \
-             1.1*Fobs_left + \
+             1.01*Fobs_left + \
              Fobs_right + \
              FOrienting() + \
              FStochastic()
