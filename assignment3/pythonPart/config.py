c7c6bd2f9d10fdf4553a1bf2929548a8c046dc1f
Xianbo XU <Silas_X@outlook.com>
2022-06-11 17:03:21 +0200

    feat(config): add configuration file

---
+++ assignment3/pythonPart/config.py
@@ -0,0 +1,8 @@
+ROBOT_IP="192.168.0.112"
+ROBOT_PORT=9559
+SPEECH_SENSITIVITY=0.4
+DIALOG_TOPICS=[]
+DIALOG_TOPICS.append("/home/nao/group_08/Topics/checkin_enu.top")  # Absolute path of the dialog topic file (on the robot).
+DIALOG_TOPICS.append("/home/nao/group_08/Topics/food_enu.top")
+DIALOG_TOPICS.append("/home/nao/group_08/Topics/general_enu.top")
+DIALOG_TOPICS.append("/home/nao/group_08/Topics/test_enu.top")
