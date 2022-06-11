ddaae31d33fc418662e1ee959245df8b44f8c5f0
Xianbo XU <Silas_X@outlook.com>
2022-06-11 17:04:19 +0200

    feat(scene_before_check_in): implement the actions of robot before checkin

---
+++ assignment3/pythonPart/Scene_before_checkin.py
@@ -0,0 +1,23 @@
+from face_detection import detect_face,onFindFace
+from start_dialog import start_dialog_on_multitopics
+import nao_nocv_2_1 as nao
+from config import ROBOT_IP,ROBOT_PORT,SPEECH_SENSITIVITY,DIALOG_TOPICS
+
+
+def meet_customer(nao):
+    while not detect_face(nao):
+        # Wait until our robot find a person
+        pass
+    onFindFace(nao) # Execuate finding face actions
+    try:
+        start_dialog_on_multitopics(ROBOT_IP,ROBOT_PORT,DIALOG_TOPICS,SPEECH_SENSITIVITY)
+    except Exception,e:
+        print "Failed to start dialog ",e
+
+
+def control_flow(nao):
+    meet_customer
+
+if __name__=="__main__":
+    nao.InitProxy(ROBOT_IP)
+    control_flow(nao)
