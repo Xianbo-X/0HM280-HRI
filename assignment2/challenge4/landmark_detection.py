08e65ce65d577391efef1ee4dfb8a56a59577fcf
YaoLu <y.lu@student.tue.nl>
2022-06-20 15:21:34 +0200

    update assignment2 for presentation

---
+++ assignment2/challenge4/landmark_detection.py
@@ -0,0 +1,41 @@
+import nao_nocv_2_1 as nao
+import numpy as np
+
+
+def search_landmark():
+    """
+    Return
+    -----
+    [Detect result, landmark angle(Yaw)]
+    """
+    # head move range -119.5 ~ 119.5 deg
+    head_mov_range = np.linspace(-1.086017,1.086017,10)
+    # detect landmark times
+    detect_cnt = 100
+    for rad in head_mov_range:
+        nao.InitTrack()
+        nao.MoveHead(yaw_val = rad, pitch_val=0, isAbsolute=True)
+#         time.sleep(0.2)
+        nao.InitVideo(1) # resolution 320*240
+        nao.InitLandMark()
+        # detect landmark and return info
+        for cnt in range(detect_cnt):
+            detected, timestamp, markerInfo=nao.DetectLandMark()
+            if(detected):
+                if(marker_ID != markerInfo[0][0]):
+                    marker_ID = markerInfo[0][0]
+                    detect_threshhold = 0
+                else:
+                    detect_threshhold += 1
+                # detected 10 times as confidence threshold
+                if(detect_threshhold>=10):
+                        nao.Say("Oh! I found the landmark!")
+                        # directly face to landmark
+                        print "Head Yaw:", nao.GetYaw(), " land mark angle: ", markerInfo[0][5]
+                        #nao.MoveHead(yaw_va = markerInfo[0][5], pitch_val=0, isAbsolute=True)
+                        nao.MoveHead(yaw_val = markerInfo[0][1]+rad, pitch_val=0, isAbsolute=True)
+                        nao.Walk(0,0,markerInfo[0][1]+rad)
+                        nao.MoveHead(yaw_val = 0, pitch_val=0, isAbsolute =True)
+                        return detected, markerInfo
+    nao.MoveHead(yaw_val = 0, pitch_val=0, isAbsolute =True)
+    return False, None
